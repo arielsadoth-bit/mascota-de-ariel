c0b20b8b83c08304ae324058670a7871407524e6
arel sadoth <arielsadoth@gmail.com>
2026-08-26 10:37:02 -0600

    Revert "Ajustar parpadeo de la mascota actualizada"
    This reverts commit f76beb7ba6b6389760bc475435d097df2641afec.

--- mascota_escritorio.py
+++ mascota_escritorio.py
@@ -134,8 +134,7 @@ class MascotaEscritorio:
         ancho, alto = parpadeo.size
         y = int(alto * 0.455)
         grosor = max(3, int(ancho * 0.018))
-        # Posiciones medidas en la imagen actual para cerrar ambos ojos parejo.
-        for centro_x in (int(ancho * 0.385), int(ancho * 0.645)):
+        for centro_x in (int(ancho * 0.40), int(ancho * 0.60)):
             radio = int(ancho * 0.065)
             dibujo.ellipse(
                 (centro_x - radio, y - radio, centro_x + radio, y + radio),
@@ -182,7 +181,7 @@ class MascotaEscritorio:
             self.y = self.base_y - paso + salto
 
             if self.tick >= self.proximo_parpadeo:
-                self.frames_parpadeo = 5
+                self.frames_parpadeo = 14
                 self.proximo_parpadeo = self.tick + 85
             if self.frames_parpadeo > 0:
                 self.canvas.itemconfigure(self.id_img, image=self.imagen_parpadeo_tk)
